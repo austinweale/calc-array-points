6604d7556f071278e6abdc6f32feb08d1886e2f0
Austin Weale <austinweale@gmail.com>
2016-10-07 13:13:17 -0700

    initialized project

---
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Austin Weale on 10/7/16.
+//  Copyright © 2016 Austin Weale. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
